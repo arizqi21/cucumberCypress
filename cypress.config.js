35421e4c8a66bc59a4b4fa2037a2ea741f1ae24d
rizqi-renos <aulia.rizqi@renos.id>
2025-06-05 19:31:01 +0700

    chore: add yaml

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const cucumber = require('cypress-cucumber-preprocessor').default;
 
 module.exports = defineConfig({
   e2e: {
+    video: true,
     specPattern: "**/*.feature",
     setupNodeEvents(on, config) {
       on("file:preprocessor", cucumber());
